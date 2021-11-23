5c2856eaf8c66ea250ca622589c63d0c92c1206b
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-23 15:55:44 +0700

    'function-ts'

---
+++ build/function.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * function sama kek js lah
+ * cuma kita mesti kasi type checking aja sih
+ */
+function getName() {
+    return 2;
+}
+console.log(getName());
+function tampaReturn() {
+    console.log('No return');
+}
+tampaReturn();
---
+++ src/function.ts
@@ -0,0 +1,15 @@
+/**
+ * function sama kek js lah
+ * cuma kita mesti kasi type checking aja sih
+ */
+
+function getName(): number{
+   return 2;
+}
+console.log(getName());
+
+function tampaReturn():void{
+   console.log('No return');
+}
+
+tampaReturn();
