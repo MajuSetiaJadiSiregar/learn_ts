9a4a89d767074e91a820d972069ea0e677c63622
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-23 14:25:52 +0700

    'tipe-data-number-boolean'

--- build/tipeData.js
+++ build/tipeData.js
@@ -1,2 +1,16 @@
 "use strict";
+/**
+ * tipe data string
+ */
 let nama = "Maju";
+nama = "siregar";
+/**
+ * tipe data number
+ */
+let umur = 25;
+umur = 23;
+/**
+ * tipe data boolean
+ */
+let isMarried = false;
+isMarried = true;
--- src/tipeData.ts
+++ src/tipeData.ts
@@ -1,1 +1,18 @@
+/**
+ * tipe data string
+ */
 let nama: string = "Maju";
+nama = "siregar";
+
+/**
+ * tipe data number
+ */
+let umur: number = 25;
+umur = 23;
+
+/**
+ * tipe data boolean
+ */
+
+let isMarried: boolean = false;
+isMarried = true;
