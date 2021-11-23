101a98256706da5c49e7d489f21c51445ae51dec
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-23 14:34:59 +0700

    'tipe-data-array'

---
+++ src/tipe_data_array.ts
@@ -0,0 +1,14 @@
+/**
+ * konsep array sama saja seperti
+ * pada pemograman sebelumnya
+ * hanya saja value array nya harus di tentukan.
+ * namun jika value array ingin bebas atau apa saja
+ * bisa masuk maka bisa menggunakan any
+ */
+
+let nilaiMatematika: number[] = [1,2,3,4];
+let daftarMataKuliah: string[];
+daftarMataKuliah = ["Bahasa Indonesia", "Matematika"];
+
+
+let profileMahasiswa = ["Lintang Ts", 25, 3.4];
