faf298108acf8388885618ccef6eb63b64ab737b
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-23 16:19:47 +0700

    'tipe-data-object'

---
+++ build/object.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * cara terbaik menggunakan
+ * object.
+ * pertama tentukan dulu type object nya
+ * kedua inject variabel dengan type yang
+ * sudah di tentukan.
+ */
+/**
+ * kedua => buat object
+ */
+let user = {
+    name: 'Lintang Js',
+    age: 20
+};
---
+++ src/object.ts
@@ -0,0 +1,22 @@
+/**
+ * cara terbaik menggunakan 
+ * object.
+ * pertama tentukan dulu type object nya
+ * kedua inject variabel dengan type yang
+ * sudah di tentukan.
+ */
+
+/**
+ * pertama
+ */
+
+type User = {name : string, age: number};
+
+/**
+ * kedua => buat object
+ */
+
+let user:User = {
+   name : 'Lintang Js',
+   age : 20
+}
