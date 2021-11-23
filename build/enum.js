403fd0ebfb783783377e0c0cad35b92c79166c17
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-23 15:48:12 +0700

    'enum'

---
+++ build/enum.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * enum =>sama konsep nya dengan enum dart
+ */
+/**
+ * numeric enum => berdasarkan index
+ * string enum => berdasarkan string
+ */
+var Month;
+(function (Month) {
+    Month[Month["JAN"] = 0] = "JAN";
+    Month[Month["FEB"] = 1] = "FEB";
+    Month[Month["MAR"] = 2] = "MAR";
+    Month[Month["APR"] = 3] = "APR";
+})(Month || (Month = {}));
+var Day;
+(function (Day) {
+    Day["Sen"] = "Senin";
+    Day["Sel"] = "Selasa";
+    Day["Ra"] = "Rabu";
+})(Day || (Day = {}));
---
+++ src/enum.ts
@@ -0,0 +1,21 @@
+/**
+ * enum =>sama konsep nya dengan enum dart
+ */
+
+/**
+ * numeric enum => berdasarkan index
+ * string enum => berdasarkan string
+ */
+
+enum Month {
+   JAN,
+   FEB,
+   MAR,
+   APR,
+}
+
+enum Day {
+   Sen = "Senin",
+   Sel = "Selasa",
+   Ra = "Rabu"
+}
